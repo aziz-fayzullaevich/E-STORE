a326afd0d1b582d82fbfee30c376a5b4e220b971
Aziz Atagullaev <azizatagullaev1@gmail.com>
2025-06-28 04:02:16 +0500

    e-store

--- src/layout/main-layout.tsx
+++ src/layout/main-layout.tsx
@@ -1,7 +1,7 @@
 import style from './main-layout-style.module.css';
 import { Outlet } from "react-router-dom"
-import Header from "../components/header/Header"
-import Footer from "../components/footer/Footer"
+import Header from "../components/header/header"
+import Footer from "../components/footer/footer"
 
 const MainLayout = () => {
     return (
